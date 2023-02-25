4ac0cd31c74fc14a3445b6451f52a7c30cc883cc
Geraldo Ignacio de Macedo <119904806+grdlondon@users.noreply.github.com>
2023-02-24 23:58:59 -0300

    Atualização de códigos
    Foram atualizados alguns códigos no JavaScript

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const dia = document.getElementById("dia");
+const Dia  = document.getElementById("dia");
 
-const horas= document.getElementById("horas");
+const Horas = document.getElementById("contador");
 
-const minutos = document.getElementById("minutos");
+const Minutos = document.getElementById("Minuto");
 
-const segundos = document.getElementById("segundos");
+const Segundos = document.getElementById("Segundo");
 
-const contagemparaonatalem = "1 Jan 2024";
+const contagemparaonatal= "1 Jan 2024";
 
 function contador(){
 
-    const contagemparaonatalemDate=NovaData("contagemparaonatalem"); 
-const dataatual= novaData();
-const totalsegundos= (contagemparaonatalem -dataatual) / 1000;
+    const contagemparaonatalData = Date(contador); 
+const dataatual = novaData();
+const totalsegundos= (contagemparaonatalData-dataatual) / 1000;
 
 const dia = Math.floor(totalsegundos/3600 / 24);
 const horas = Math.floor(totalsegundos / 3600) % 24;
@@ -29,10 +29,10 @@ segundos.innerHTML= formatTime(segundos);
 }
 
 function formatTime(time){
-    return time < 10 ? "o${time}": time
+    return time < 10 ? "0${time}": time
 }
 
-contador();
+ contador();
 
 setInterval(contador,1000);
 
